0471a75e083a88305f7db3290c6b557cf12520a5
Joe Mattie <JoeMattie@users.noreply.github.com>
2016-05-20 00:55:18 -0800

    removed extra code (#2)

--- index.ios.js
+++ index.ios.js
@@ -9,298 +9,96 @@ import {
 } from 'react-native';
 
 var SpellBook = React.createClass({
+    
     getInitialState: function(){
-        return {
-            activated1: false,
-            activated2: false,
-            activated3: false,
-            activated4: false,
-            activated5: false,
-            activated6: false,
-            activated7: false,
-            activated8: false,
-            activated9: false,
-            activated10: false,
-            activated11: false,
-            activated12: false
+        return { 
+            slots: Array(12).fill(false)
         }
     },
-    render: function(){
-        return <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headText}>
-                    Spell Book
-                </Text>
-            </View>
-            <View style={styles.body}>
-                <Text style={styles.footText}>
-                    Dungeon
-                </Text>
-            </View>
-            <View style={styles.slot}>
-                <View style={styles.slot}>
-                    {this.toggleSlot1()}
-                    {this.toggleSlot2()}
-                    {this.toggleSlot3()}
-                    {this.toggleSlot4()}
+
+    render: function() {
+
+        const slotRow1 = this.state.slots.slice(0, 4).map( ( slot, i ) => {
+            const style = this.state.slots[i] ? styles.buttonOn : styles.buttonOff;
+            return (
+                <TouchableHighlight key={ i } underlayColor="gray" onPress={ () => { this.handleActivation( i ) } }  style={ style }>
+                    <Text> {this.state.slots[slot] ? 'On' : 'Off'} </Text>
+                </TouchableHighlight>
+            )
+        });
+
+        const slotRow2 = this.state.slots.slice(4, 8).map( ( slot, i ) => {
+            const j = i + 4;
+            const style = this.state.slots[j] ? styles.buttonOn : styles.buttonOff;
+            return (
+                <TouchableHighlight key={ j } underlayColor="gray" onPress={ () => { this.handleActivation( j ) } }  style={ style }>
+                    <Text> {this.state.slots[slot] ? 'On' : 'Off'} </Text>
+                </TouchableHighlight>
+            )
+        });
+        
+        const slotRow3 = this.state.slots.slice(8, 12).map( ( slot, i ) => {
+            const j = i + 8;
+            const style = this.state.slots[j] ? styles.buttonOn : styles.buttonOff;
+            return (
+                <TouchableHighlight key={ j } underlayColor="gray" onPress={ () => { this.handleActivation( j ) } }  style={ style }>
+                    <Text> {this.state.slots[slot] ? 'On' : 'Off'} </Text>
+                </TouchableHighlight>
+            )
+        });
+
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headText}>
+                        Spell Book
+                    </Text>
                 </View>
-            </View>
-            <View style={styles.slot}>
-                <View style={styles.slot}>
-                    {this.toggleSlot5()}
-                    {this.toggleSlot6()}
-                    {this.toggleSlot7()}
-                    {this.toggleSlot8()}
+                <View style={styles.body}>
+                    <Text style={styles.footText}>
+                        Dungeon
+                    </Text>
                 </View>
-            </View>
-            <View style={styles.slot}>
                 <View style={styles.slot}>
-                    {this.toggleSlot9()}
-                    {this.toggleSlot10()}
-                    {this.toggleSlot11()}
-                    {this.toggleSlot12()}
+                    <View style={styles.slot}>{ slotRow1 }</View>
                 </View>
-            </View>
-            <View style={styles.manaBar}>
-                <Text style={styles.footText}>
-                    Mana Bar
-                </Text>
-            </View>
-            <View style={styles.footer}>
+                <View style={styles.slot}>
+                    <View style={styles.slot}>{ slotRow2 }</View>
+                </View>
+                <View style={styles.slot}>
+                    <View style={styles.slot}>{ slotRow3 }</View>
+                </View>
+                <View style={styles.manaBar}>
+                    <Text style={styles.footText}>Mana Bar</Text>
+                </View>
                 <View style={styles.footer}>
-                    {this.clearButton()}
-                    {this.castButton()}
+                    <View style={styles.footer}>
+                        <TouchableHighlight underlayColor="gray" onPress={ this.commitClear } style={ styles.button }>
+                            <Text>Clear</Text>
+                        </TouchableHighlight>
+                        <TouchableHighlight underlayColor="gray" onPress={ this.commitCast } style={ styles.button }>
+                            <Text>Cast</Text>
+                        </TouchableHighlight>
+                    </View>
                 </View>
             </View>
+        )
+    },
 
-        </View>
+    handleActivation: function(slot){
+        let newslots = this.state.slots.slice(0, this.state.slots.length );
+        newslots[slot] = !this.state.slots[slot];
+
+        this.setState({ slots: newslots });        
     },
-    toggleSlot1: function(){
-        var style = this.state.activated1 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation1} style={style}>
-            <Text>
-                {this.state.activated1 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot2: function(){
-        var style = this.state.activated2 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation2} style={style}>
-            <Text>
-                {this.state.activated2 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot3: function(){
-        var style = this.state.activated3 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation3} style={style}>
-            <Text>
-                {this.state.activated3 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot4: function(){
-        var style = this.state.activated4 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation4} style={style}>
-            <Text>
-                {this.state.activated4 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot5: function(){
-        var style = this.state.activated5 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation5} style={style}>
-            <Text>
-                {this.state.activated5 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot6: function(){
-        var style = this.state.activated6 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation6} style={style}>
-            <Text>
-                {this.state.activated6 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot7: function(){
-        var style = this.state.activated7 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation7} style={style}>
-            <Text>
-                {this.state.activated7 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot8: function(){
-        var style = this.state.activated8 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation8} style={style}>
-            <Text>
-                {this.state.activated8 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot9: function(){
-        var style = this.state.activated9 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation9} style={style}>
-            <Text>
-                {this.state.activated9 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot10: function(){
-        var style = this.state.activated10 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation10} style={style}>
-            <Text>
-                {this.state.activated10 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot11: function(){
-        var style = this.state.activated11 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation11} style={style}>
-            <Text>
-                {this.state.activated11 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    toggleSlot12: function(){
-        var style = this.state.activated12 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.handleActivation12} style={style}>
-            <Text>
-                {this.state.activated12 ? 'On' : 'Off'}
-            </Text>
-        </TouchableHighlight>
-    },
-    handleActivation1: function(){
-        if(this.state.activated1 == false){
-            this.setState({activated1: true});
-            return
-        }else{
-            this.setState({activated1: false});
-        }
-    },
-    handleActivation2: function(){
-        if(this.state.activated2 == false){
-            this.setState({activated2: true});
-            return
-        }else{
-            this.setState({activated2: false});
-        }
-    },
-    handleActivation3: function(){
-        if(this.state.activated3 == false){
-            this.setState({activated3: true});
-            return
-        }else{
-            this.setState({activated3: false});
-        }
-    },
-    handleActivation4: function(){
-        if(this.state.activated4 == false){
-            this.setState({activated4: true});
-            return
-        }else{
-            this.setState({activated4: false});
-        }
-    },
-    handleActivation5: function(){
-        if(this.state.activated5 == false){
-            this.setState({activated5: true});
-            return
-        }else{
-            this.setState({activated5: false});
-        }
-    },
-    handleActivation6: function(){
-        if(this.state.activated6 == false){
-            this.setState({activated6: true});
-            return
-        }else{
-            this.setState({activated6: false});
-        }
-    },
-    handleActivation7: function(){
-        if(this.state.activated7 == false){
-            this.setState({activated7: true});
-            return
-        }else{
-            this.setState({activated7: false});
-        }
-    },
-    handleActivation8: function(){
-        if(this.state.activated8 == false){
-            this.setState({activated8: true});
-            return
-        }else{
-            this.setState({activated8: false});
-        }
-    },
-    handleActivation9: function(){
-        if(this.state.activated9 == false){
-            this.setState({activated9: true});
-            return
-        }else{
-            this.setState({activated9: false});
-        }
-    },
-    handleActivation10: function(){
-        if(this.state.activated10 == false){
-            this.setState({activated10: true});
-            return
-        }else{
-            this.setState({activated10: false});
-        }
-    },
-    handleActivation11: function(){
-        if(this.state.activated11 == false){
-            this.setState({activated11: true});
-            return
-        }else{
-            this.setState({activated11: false});
-        }
-    },
-    handleActivation12: function(){
-        if(this.state.activated12 == false){
-            this.setState({activated12: true});
-            return
-        }else{
-            this.setState({activated12: false});
-        }
-    },
-    castButton: function(){
-        return <TouchableHighlight underlayColor="gray" onPress={this.commitCast} style={styles.button}>
-            <Text>
-                Cast
-            </Text>
-        </TouchableHighlight>
-    },
-    clearButton: function(){
-        // var style = this.state.activated1 ? styles.buttonOn : styles.buttonOff;
-        return <TouchableHighlight underlayColor="gray" onPress={this.commitClear} style={styles.button}>
-            <Text>
-                Clear
-            </Text>
-        </TouchableHighlight>
-    },
+
     commitClear: function(){
-        this.setState({activated1: false});
-        this.setState({activated2: false});
-        this.setState({activated3: false});
-        this.setState({activated4: false});
-        this.setState({activated5: false});
-        this.setState({activated6: false});
-        this.setState({activated7: false});
-        this.setState({activated8: false});
-        this.setState({activated9: false});
-        this.setState({activated10: false});
-        this.setState({activated11: false});
-        this.setState({activated12: false});
-
+        this.setState({ slots: Array(12).fill(false) });
     },
+
     commitCast: function(){
         //pushes each cells # to a string..
         this.commitClear()
-
     }
 });
 
